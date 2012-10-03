232db65a546947c29a4c9fb6655726bc81cb2bb6
vkadam <vishal.kadam@contractor.cengage.com>
2012-10-03 16:44:15 -0400

    Completed pom changes to run jasmine test (In browser only)

--- src/js/jiraTracker.js
+++ src/js/jiraTracker.js
@@ -10,12 +10,10 @@
     })
 })(jQuery);
 
-$(function() {
-
-});
+$(function() {});
 
 function jiraClient() {
-    /*$.ajax({
+/*$.ajax({
         url : 'http://jira.cengage.com/rest/api/2/search?jql=assignee="vishal.kadam"',
         headers : { "Authorization": "Basic " + Base64.encode('vishal.kadam:cengagejira')}
       })
@@ -29,20 +27,24 @@ function jiraClient() {
     //GSLoader.loadSheet({key : "0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c", debug: true})
     var urls = {
         // "Spreadsheets" : "https://spreadsheets.google.com/feeds/spreadsheets/private/full",
-        //"list_basic" : "https://spreadsheets.google.com/feeds/list/0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c/od6/private/basic",
+        // "list_basic" : "https://spreadsheets.google.com/feeds/list/0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c/od6/private/basic",
         // "Private_Basic" : "https://spreadsheets.google.com/feeds/worksheets/0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c/private/basic",
-        //"Private_Full" : "https://spreadsheets.google.com/feeds/worksheets/0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c/private/full",
-        //"list_full": "https://spreadsheets.google.com/feeds/list/0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c/od6/private/full"
+        // "Private_Full" : "https://spreadsheets.google.com/feeds/worksheets/0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c/private/full",
+        "list_full": "https://spreadsheets.google.com/feeds/list/0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c/od6/private/full"
     }
 
-    $.each(urls, function(key, value) {
+    /*$.each(urls, function(key, value) {
         $.ajax({
             url: value
         }).done(function(data, textStatus, jqXHR) {
-            console.log(key, value, data);
-            //console.log(jqXHR.responseText);
+            //console.log(key, value, data);
+            console.log(jqXHR.responseText);
         });
-    })
+    })*/
+
+    GSLoader.loadSpreadsheet("0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c").done(function(spreadsheet){
+        console.log(spreadsheet);
+    });
 }
 
 $(function() {
