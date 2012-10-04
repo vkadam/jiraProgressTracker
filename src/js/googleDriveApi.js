87371faa37e64e2dfe2dcecc1948b627649a0000
vkadam <vishal.kadam@contractor.cengage.com>
2012-10-04 16:37:32 -0400

    DONE: Creating google spreadsheet and worksheet using google drive

---
+++ src/js/googleDriveApi.js
@@ -0,0 +1,81 @@
+var googleClientApi = new(function() {
+    var CLIENT_ID = '1074663392007.apps.googleusercontent.com';
+    var SCOPES = ["https://www.googleapis.com/auth/drive", "https://spreadsheets.google.com/feeds"].join(" ");
+    var _this = this;
+
+    this.onLoad = function() {
+        gapi.client.load('drive', 'v2', googleClientApi.drive.onLoad);
+        googleClientApi.checkAuth();
+    },
+
+    this.checkAuth = function() {
+        gapi.auth.authorize({
+            'client_id': CLIENT_ID,
+            'scope': SCOPES,
+            'immediate': true
+        }, _this.handleAuthResult);
+    },
+
+    this.handleAuthResult = function(authResult) {
+        if (authResult && !authResult.error) {
+            // Access token has been successfully retrieved, requests can be sent to the API.
+            console.log("Google Api Authentication Succeed", authResult)
+            // var filePicker = document.getElementById('filePicker');
+            // filePicker.style.visibility = '';
+            // filePicker.onchange = uploadFile;
+        } else {
+            console.log("Authenticating Google Api")
+            // No access token could be retrieved, force the authorization flow.
+            gapi.auth.authorize({
+                'client_id': CLIENT_ID,
+                'scope': SCOPES,
+                'immediate': false
+            }, _this.handleAuthResult);
+        }
+    }
+
+})();
+
+googleClientApi.drive = {
+    onLoad: function() {
+        googleClientApi.checkAuth();
+    },
+
+    createSpreadSheet: function(fileTitle) {
+        var request = gapi.client.request({
+            'path': '/drive/v2/files',
+            'method': 'POST',
+            'body': {
+                "title": fileTitle,
+                "mimeType": "application/vnd.google-apps.spreadsheet"
+            }
+        });
+
+        request.execute(function(resp) {
+            console.log(resp);
+        });
+    },
+
+    retrieveAllFiles: function(callback) {
+        var retrievePageOfFiles = function(request, result) {
+                request.execute(function(resp) {
+                    result = result.concat(resp.items);
+                    var nextPageToken = resp.nextPageToken;
+                    if (nextPageToken) {
+                        request = gapi.client.drive.files.list({
+                            'pageToken': nextPageToken
+                        });
+                        retrievePageOfFiles(request, result);
+                    } else {
+                        if (callback) {
+                            callback.apply(callback, result);
+                        };
+                        return result;
+                    }
+                });
+            }
+        var initialRequest = gapi.client.drive.files.list();
+        retrievePageOfFiles(initialRequest, []);
+    }
+
+}
--- src/js/jiraTracker.js
+++ src/js/jiraTracker.js
@@ -30,29 +30,88 @@ function jiraClient() {
         // "list_basic" : "https://spreadsheets.google.com/feeds/list/0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c/od6/private/basic",
         // "Private_Basic" : "https://spreadsheets.google.com/feeds/worksheets/0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c/private/basic",
         // "Private_Full" : "https://spreadsheets.google.com/feeds/worksheets/0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c/private/full",
-        "list_full": "https://spreadsheets.google.com/feeds/list/0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c/od6/private/full"
+        // "list_full": "https://spreadsheets.google.com/feeds/list/0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c/od6/private/full"
+        // "document_list_api": "https://docs.google.com/feeds/default/private/full"
     }
 
-    /*$.each(urls, function(key, value) {
+    $.each(urls, function(key, value) {
         $.ajax({
             url: value
         }).done(function(data, textStatus, jqXHR) {
             //console.log(key, value, data);
             console.log(jqXHR.responseText);
         });
-    })*/
-    
-    var spreadsheets = ["0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c", 
-        "0AquDXlXxVjqPdElEQ3RSTzZ5SG4zVUN5UWQzYnZQbnc"];
-    $.each(spreadsheets, function(key, value) {
-        GSLoader.loadSpreadsheet(value)
-        .done(function(spreadsheet){
-            console.log(this.title, this);
-        });
     })
 
+    var spreadsheets = [
+    // "0AlpsUVqaDZHSdG4yR2hXZjJpbmRNS2s3RTU4eVQyQ2c", 
+    // "0AquDXlXxVjqPdElEQ3RSTzZ5SG4zVUN5UWQzYnZQbnc"
+    ];
+    $.each(spreadsheets, function(key, value) {
+        GSLoader.loadSpreadsheet(value).done(function(spreadsheet) {
+            console.log(this.title, this);
+        });
+    });
+
+
+    var url = "https://docs.google.com/feeds/default/private/full"
+
+/*var postdata = '<?xml version="1.0" encoding="UTF-8"?>
+        <entry xmlns="http://www.w3.org/2005/Atom" xmlns:docs="http://schemas.google.com/docs/2007"> +
+          <!-- Replace the following line appropriately to create another type of resource. -->
+          <category scheme="http://schemas.google.com/g/2005#kind"
+              term="http://schemas.google.com/docs/2007#spreadsheet"/>
+          <title>Spreadsheet By Document List Api</title>
+        </entry>'*/
+
+}
+
+function OnLoadCallback() {
+    gapi.client.load('drive', 'v2', function() {
+
+        var request = gapi.client.request({
+            'path': '/drive/v2/files',
+            'method': 'POST',
+            'body': {
+                "title": "cat.jpg",
+                "mimeType": "image/jpeg",
+                "description": "Some"
+            }
+        });
+        request.execute(function(resp) {
+            console.log(resp);
+        });
+    });
 }
 
 $(function() {
     jiraClient();
+
+    $(".create-sheet").click(function() {
+        $.ajax({
+            url: "https://spreadsheets.google.com/feeds/worksheets/0AlpsUVqaDZHSdGh3bzViTEFmbVVQYTI5VkdLYW8zWVE/private/full",
+            type: "POST",
+            contentType: "application/atom+xml",
+            headers: {
+                "GData-Version": "3.0"
+            },
+            data:'<entry xmlns="http://www.w3.org/2005/Atom" '+
+                    'xmlns:gs="http://schemas.google.com/spreadsheets/2006">'+
+                    '<title>Expenses</title>'+
+                    '<gs:rowCount>50</gs:rowCount>'+
+                    '<gs:colCount>10</gs:colCount>'+
+                '</entry>'
+        }).done(function(data, textStatus, jqXHR) {
+            //console.log(key, value, data);
+            console.log(jqXHR.responseText);
+        });
+    })
 });
+
+/**
+ * Called when the client library is loaded.
+ */
+
+window.googleDrieClientLoaded = function() {
+    googleClientApi.onLoad();
+}
